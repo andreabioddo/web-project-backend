7a73b126f052faa10e2f4dbb54e7b88f6434d033
skrhakv <v.skrhak@gmail.com>
2023-01-15 21:16:42 +0100

    Update bash script

--- create-database.sh
+++ create-database.sh
@@ -4,3 +4,4 @@
 export $(grep -v '^#' .env | xargs)
 
 sudo PGPASSWORD=$DB_USER_PASSWD psql -U $DB_USER -h 127.0.0.1 -d $DB_NAME -a -f create-database.sql
+sudo PGPASSWORD=$DB_USER_PASSWD psql -U $DB_USER -h 127.0.0.1 -d $DB_NAME -a -f dummy-data.sql
